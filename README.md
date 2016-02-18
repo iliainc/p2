2719aa1897d3c046e3f724f324cc207c63321535
Ilya <inossov@fas.harvard.edu>
2016-02-18 06:49:05 +0000

    structural setup

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Hello World
-02/15/16
-Just for practice
+# Project 2
+
+## Live URL
+<http://p2.ilyanossov.me>
+
+## Description
+This project sets the stage for listing future projects in a portfolio fashion, while leveraging the Bootstrap framework to build a responsive website. Included are placeholders for the links which will point to future projects as they're completed.
+
+## Demo
+<https://youtu.be/pFtJC-CaPTM>
+
+## Details for teaching team
+No login required.
+
+## Outside code
+* Basic HTML Structure: http://www.w3schools.com/html/
+* Basic CSS Structure: http://www.w3schools.com/html/html_css.asp
+* CSS Div Spacing: http://stackoverflow.com/questions/11014724/space-between-two-divs
+* Footer Alignment: http://stackoverflow.com/questions/15725098/how-to-align-footer-text-on-the-right-and-on-the-left
+* Menu Link Anchors: http://www.w3schools.com/html/html_links.asp
+* Bootstrap: http://getbootstrap.com/getting-started/
+* Bootstrap Example: http://getbootstrap.com/examples/carousel/
--- index.php
+++ index.php
@@ -1,1 +1,70 @@
-<h1>Hello World!</h1>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>CSCI E-15 | Project 2</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <div class="navbar-wrapper">
+  <div class="container">
+
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project 2</a>
+        </div>
+      </div>
+    </nav>
+
+  </div>
+</div>
+
+  <div class="pwGen">
+
+    <div class="name">
+      <h1>Password Generator</h1>
+    </div>
+
+    <div class="password">
+      <div class="row">
+        <div class="col-center">
+
+          <p>Amount of Words: <a href="#">TODO</a></p>
+          <p>Add a Number: <a href="#">TODO</a></p>
+          <p>Add a Number: <a href="#">TODO</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Generate My Password! &raquo;</a></p>
+
+        </div>
+      </div>
+    </div>
+
+    <img src='images/password_strength.png' alt='Password Generator'>
+
+  </div>
+
+  <div class="footer">
+      <p class="pull-right"><a href="#">Back to top</a></p>
+      <p>&copy; 2016 Ilya Nossov</p>
+  </div>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
