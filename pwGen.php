dcf8b6d3dcc84ba68977ea2e01f48add79209487
Ilya <inossov@fas.harvard.edu>
2016-02-18 08:33:05 +0000

    Loading word and symbol list

--- pwGen.php
+++ pwGen.php
@@ -3,8 +3,8 @@
     # declare array to hold our list of words
     $wordList = [];
 
-    # load words into memory
-    $words = 'words.txt';
+    # load words
+    $words = 'words/words.txt';
     $data = file($words);
 
     # build array full of words provided in text file
@@ -13,6 +13,31 @@
     }
 
     # print out array values for debugging
-    print_r($wordList);
+    # print_r($wordList);
+
+    # declare array to hold our list of symbols
+    $symbolList = [];
+
+    # load symbols
+    $symbols = 'words/symbols.txt';
+    $data = file($symbols);
+
+    # build array full of symbols provided in text file
+    foreach($data as $symbol) {
+        array_push($symbolList, $symbol);
+    }
+
+    # print out array values for debugging
+    # print_r($symbolList);
+
+    # generate random integers to be used as indices to retrieve array values, corresponding to
+    # how many symbols and words there are in the input .txt files
+    $intRandWordIndex = rand(0,44);
+    $intRandSymbolIndex = rand(0,11);
+
+    # generate random number to be used if user requests number in password
+    $intRandNumber = rand(0,9999);
+
+    
 
 ?>
